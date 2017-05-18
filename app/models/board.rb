92fcf65cebe855ad18de46ac21f149fdb63acd60
Emily Chai <echai93@gmail.com>
2017-05-18 11:59:07 +0100

    Board model initalised with 9 fields

---
+++ app/models/board.rb
@@ -0,0 +1,12 @@
+class Board
+
+  attr_reader :fields
+
+  def initalize
+    @fields = [
+                nil, nil ,nil,
+                nil, nil, nil,
+                nil, nil, nil
+              ]
+  end
+end
