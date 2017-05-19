9c6896d8bff468203ddd9fb18ffb9c4ceb4fb1d3
Emily Chai <echai93@gmail.com>
2017-05-19 11:47:31 +0100

    Tests game is initalized with players ad a board

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,14 +1,26 @@
 require './app/models/game'
+require './app/models/player'
 
 describe Game do
-  subject(:game) { described_class.new }
+  let(:player_X) { double Player.new("X") }
+  let(:player_O) { double Player.new("O") }
+  let(:board) { Board.new }
+  subject(:game) { described_class.new(player_X, player_O, board) }
 
   context 'New game' do
-    describe '#initalize' do
-      it 'is initalized with an empty board' do
-        expect(game.board).to eq nil
+    describe '#initialize' do
+      it 'is initialized with a board' do
+        expect(game.board).to eq board
+      end
+      it 'is initialized with player_X' do
+        expect(game.player_X).to eq player_X
+      end
+      it 'is initialized with player_O' do
+        expect(game.player_O).to eq player_O
       end
     end
   end
 
+
+  
 end
